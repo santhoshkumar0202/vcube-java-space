17f925b9d7fcecba35dbf6691ef528d06b6b1dc9
santhoshkumar <bestsanthoshkumar@gmail.com>
2025-08-24 13:30:29 +0530

    gitcheckclasschnged

---
+++ javaconfigproject/src/main/java/com/vcube/javaconfigproject/GitCheckclasss.java
@@ -0,0 +1,14 @@
+package com.vcube.javaconfigproject;
+
+import java.io.Console;
+
+public class GitCheckclasss {
+
+	public static void main(String[] args) {
+		System.out.println("git check class working properly");
+		System.out.println("status check class working properly");
+		System.out.println("added check class working properly");
+		
+		
+	}
+}
