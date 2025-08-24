a1afd9af1cedc7471dd2805fb5dd17bbdbc6d68c
santhoshkumar <bestsanthoshkumar@gmail.com>
2025-08-24 14:19:24 +0530

    removed all the code

--- javaconfigproject/src/main/java/com/vcube/javaconfigproject/GitCheckclasss.java
+++ javaconfigproject/src/main/java/com/vcube/javaconfigproject/GitCheckclasss.java
@@ -5,9 +5,7 @@ import java.io.Console;
 public class GitCheckclasss {
 
 	public static void main(String[] args) {
-		System.out.println("git check class working properly");
-		System.out.println("status check class working properly");
-		System.out.println("added check class working properly");
+		
 		
 		
 	}
